9284ae75a0deaca67052bf2cdc6306f6059104f9
plmphuc <plm.phuc@tego.global>
2024-07-04 14:47:02 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      mainColor: "#fbf8f6",
+      primaryColor: "#F3EBE5",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
